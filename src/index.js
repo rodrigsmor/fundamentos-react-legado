e2802c76f8ebe05af162ea4adffa37e4245dfff2
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2022-03-24 16:34:18 -0300

    Returns two components in one file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MultiElementos from './components/MultiElementos';
+import { CompA, CompB } from './components/DoisComponentes';
 import PrimeiroComponente from './components/PrimeiroComponente';
 
 ReactDOM.render(
     <div>
+        <MultiElementos />
+        <CompA valor={'Olá, eu sou A!'} />
+        <CompB valor={'B NA ÁREA!'} />
         <PrimeiroComponente valor={'Bom dia!'} />
     </div>,
     document.getElementById('root')
